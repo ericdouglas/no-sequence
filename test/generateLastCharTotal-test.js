cbe8e0aea9f789b55139c1b97da1ed804c210f59
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-25 18:59:19 -0200

    more tests in the generateLastCharTotal method

--- test/generateLastCharTotal-test.js
+++ test/generateLastCharTotal-test.js
@@ -5,4 +5,12 @@ describe( '#generate last char total', function() {
   it( 'should be equal to the last charcode multiplied by the length', function() {
     expect( lastCharTotal( '123456' )).to.equal( 324 );
   });
+
+  it( 'should be equal to the last letter(charcode) multiplied by the length', function() {
+    expect( lastCharTotal( 'abcdef' )).to.equal( 612 );
+  });
+
+  it( 'should be equal to the last capital letter(charcode) multiplied by the length', function() {
+    expect( lastCharTotal( 'ABCDEF' )).to.equal( 420 );
+  });
 });
