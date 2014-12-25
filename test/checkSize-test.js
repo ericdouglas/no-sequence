5335b5a290ca187857574d14da1fe8099c4eab68
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-25 19:08:24 -0200

    more test checkSize method

--- test/checkSize-test.js
+++ test/checkSize-test.js
@@ -5,4 +5,8 @@ describe( '#checkSize', function() {
   it( 'should return false if the password is less than minimum size', function() {
     expect( checkSize( '12345', 6 )).to.be.false;
   });
+
+  it( 'should return true if the password equal the minimum size', function() {
+    expect( checkSize( '123456', 6 )).to.be.true;
+  });
 });
