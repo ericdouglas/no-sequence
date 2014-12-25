fec60ed90a18d7134b36f37f82e76d33f7ee1e9d
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-25 19:33:55 -0200

    create generateMaxValue method

--- utils/generateMaxValue.js
+++ utils/generateMaxValue.js
@@ -1,5 +1,25 @@
 function generateMaxValue( pass ) {
 
+  /**
+  *
+  * Sum each value of the 'pass' like they are in sequence
+  *
+  * @param {String} pass : password
+  *
+  **/
+
+  var total = 0;
+
+  var splitPass = pass.split( '' );
+  var length = pass.length;
+
+  splitPass
+    .forEach( function( digit, index ) {
+      total += digit.charCodeAt() + length - ( index + 1 );
+    });
+
+  return total;
+
 }
 
 module.exports = generateMaxValue;
