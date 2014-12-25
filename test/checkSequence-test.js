d2e5e9e843d817495054066d96f13e0f61b1de01
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-25 21:00:12 -0200

    test if checkSequence return false when a pass are letters - ascending order

--- test/checkSequence-test.js
+++ test/checkSequence-test.js
@@ -2,15 +2,19 @@ var checkSequence = require( '../utils/checkSequence' );
 var expect        = require( 'chai' ).expect;
 
 describe( '#checkSequence', function() {
-  it( 'should return false if the pass do not have enough chars', function() {
+  it( 'should return false if the password do not have enough chars', function() {
     expect( checkSequence( '12345', 6 )).to.be.false;
   });
 
-  it( 'should return false if the pass is in ascending order', function() {
+  it( 'should return false if the password is in ascending order', function() {
     expect( checkSequence( '123456', 6 )).to.be.false;
   });
 
-  it( 'should return false if the pass is in descending order', function() {
+  it( 'should return false if the password is in descending order', function() {
     expect( checkSequence( '987654', 6 )).to.be.false;
+  });
+
+  it( 'should return false if the password are letters and are in ascending order', function() {
+    expect( checkSequence( 'defghi', 6 )).to.be.false;
   });
 });
