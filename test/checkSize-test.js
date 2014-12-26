d867925713728209deeeeb05f8cac34bf1dc201e
ericdouglas <eric.douglas.mail@gmail.com>
2014-12-26 07:42:06 -0200

    more one test in checkSize

--- test/checkSize-test.js
+++ test/checkSize-test.js
@@ -1,7 +1,7 @@
 var checkSize = require( '../utils/checkSize' );
 var expect    = require( 'chai' ).expect;
 
-describe( '#checkSize', function() {
+describe.only( '#checkSize', function() {
   it( 'should return false if the password is less than minimum size', function() {
     expect( checkSize( '12345', 7 )).to.be.false;
   });
@@ -12,5 +12,9 @@ describe( '#checkSize', function() {
 
   it( 'should return true if the password is bigger than the minimum size', function() {
     expect( checkSize( '123456789', 7 )).to.be.true;
+  });
+
+  it( 'should return false if the password are chars and digits and is less than minimum size', function() {
+    expect( checkSize( 'a23def', 3 )).to.be.true;
   });
 });
